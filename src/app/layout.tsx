1ad9b46f157b8be3f61d49feddc21408e11e2cc4
Lucas Pereira <44583320+pereira299@users.noreply.github.com>
2024-09-07 14:19:44 -0300

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,6 +67,31 @@ export default function RootLayout({
           type="image/<generated>"
           sizes="<generated>"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5126805,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+          }}
+        ></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-18YT47LENN"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-18YT47LENN');`,
+          }}
+        ></script>
       </head>
       <body
         className={`${bitter.variable} ${lato.variable} ${gloria.variable} dark font-lato overflow-x-hidden bg-black`}
